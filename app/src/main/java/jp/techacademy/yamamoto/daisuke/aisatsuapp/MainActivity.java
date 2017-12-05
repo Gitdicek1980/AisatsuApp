e7b134847bfe0bc521bc85cc60873e8acf937fca
d_yamamoto <nice_day81@yahoo.co.jp>
2017-12-05 13:41:48 +0900

    課題

---
+++ app/src/main/java/jp/techacademy/yamamoto/daisuke/aisatsuapp/MainActivity.java
@@ -0,0 +1,49 @@
+package jp.techacademy.yamamoto.daisuke.aisatsuapp;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.button1) {
+                    showTimetDialog();
+                }
+            }
+            private void showTimetDialog() {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if(hourOfDay <=9) {
+                            textView.setText("おはよう");
+                        }else if(hourOfDay <= 17) {
+                            textView.setText("こんにちは");
+                        }else if(hourOfDay <=1){
+                            textView.setText("こんばんは");
+                        }
+                    }
+                }, 0, 0, true);
+                timePickerDialog.show();
+            }
+        });
+    }
+
+
+}
